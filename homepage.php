d7761fd0d75e57513fa378895f1096d60b6f7936
unknown <dhanna@alphaworks.tech>
2022-07-24 02:02:59 -0700

    Updating homepage and Footer

---
+++ homepage.php
@@ -0,0 +1,18 @@
+<?php 
+  /**
+   * Template Name: Homepage
+   * 
+   * @package Portfolio1
+   * 
+   */
+?>
+
+<?php get_header(); ?>
+
+/* homepage html code goes here */
+
+<?php get_footer(); ?>
+
+<?php
+get_template_part('template-parts/name-of-template-parts-file-here')
+?>
